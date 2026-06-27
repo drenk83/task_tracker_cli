716217f03d825607900ac415e9ea03eae207e45a
drenk83 <ytdrenk@gmail.com>
2026-06-27 18:16:59 +0300

    config

---
+++ config/config.go
@@ -0,0 +1,15 @@
+package config
+
+const (
+	Add    = "add"
+	Update = "update"
+	Delete = "delete"
+	List   = "list"
+
+	MarkInProgress = ""
+	MarkDone       = ""
+
+	StatusDone       = ""
+	StatusTodo       = ""
+	StatusInProgress = ""
+)
--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	cfg "github.com/drenk83/task_tracker_cli/config"
+)
+
+const (
+	ADD    = "add"
+	UPDATE = "update"
+	DELETE = "delete"
+)
 
 func main() {
-	fmt.Println("Hellow, world!")
+	args := os.Args[1:]
+	if len(args) == 0 {
+		ProgramInstruction()
+		os.Exit(0)
+	}
+
+	switch args[0] {
+	case cfg.Add:
+		Add()
+	case cfg.Update:
+		Update()
+	case cfg.Delete:
+		Delete()
+	case cfg.List:
+		List()
+	default:
+		fmt.Println("Unknow arguments")
+	}
+}
+
+func ProgramInstruction() {
+	fmt.Println("No argumetns")
+}
+
+func Add() {
+	fmt.Println("Add argument")
+}
+
+func Update() {
+	fmt.Println("Update argument")
+}
+
+func Delete() {
+	fmt.Println("Delete argument")
+}
+
+func List() {
+	fmt.Println("List argument")
 }
